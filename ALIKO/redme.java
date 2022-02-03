fc5ecb4af8957888677d83e6a3bcc65c5717339f
unknown <root@root.root>
2022-02-03 17:07:17 +0200

    DAWALA

---
+++ ALIKO/redme.java
@@ -0,0 +1,93 @@
+PROCESSUS
+* Install  laravel ( new project ) (in folder)
+* download template
+*
+
+root > web > {
+    return view("/home")
+}
+
+.env { les parametre
+    les nom s
+    en local/production
+
+    this file ne se synchronise pas
+    Eviter de reconfigurer les project
+    Eviter de reconfigurer les
+
+    laravel 8.7
+
+    .env on google
+}
+
+metre les fichiers sur des serveurs different
+pardefaut ils sont dans public
+
+@assets("vendor,") {
+    permet d'heberger les fichier sur diff serveur
+    loading assets from blade
+    (blade 
+    eliminer certain code html and le fichier php
+    )
+}
+
+config > app  {
+    metre
+    
+}
+asset_url {
+    obtenir les fichier a partir d'une url
+}
+
+
+
+inclure
+@extendsection
+
+@endsectiion
+
+
+
+BASE DE DONNES
+> les parametre sur les users
+> users
+ ----- Save all in one folder
+
+
+
+
+ ---- Git hub
+ Token 
+ > cogs > developer ...  
+ 
+ set up stream > specifier la branche
+ ~# branche master
+ git branche ( sur la quel je me reouve
+ ) 
+
+
+
+
+
+
+PROJET
+v1
+Nous rassurer qu'un utilisateur publie une évitation
+- Creer comptes user
+
+
+Tache
+ - chaqu'un va pull un projet sur son pc
+ - cloner le projet
+ - Composer update
+ - git ignore
+
+ Faire une push avec
+  * ssh (generer une clé privé, public,)
+  * Token
+
+Composer update
+
+
+is probleme
+>> metre dans un issus
